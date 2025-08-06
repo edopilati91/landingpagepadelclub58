e82c76aa57b90d61781d1c0ebe5e182d5138a2db
Edoardo Pilati <pilati.edoardo@gmail.com>
2025-08-06 17:19:08 +0200

    Update Footer.tsx
    messo link instagram corretto

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => {
               <a href="#" className="w-10 h-10 bg-padel-blue rounded-full flex items-center justify-center hover:bg-padel-blue-light transition-colors">
                 <Facebook className="w-5 h-5" />
               </a>
-              <a href="#" className="w-10 h-10 bg-padel-blue rounded-full flex items-center justify-center hover:bg-padel-blue-light transition-colors">
+              <a href="https://www.instagram.com/padel.58/" target="_blank" rel="noopener noreferrer" className="w-10 h-10 bg-padel-blue rounded-full flex items-center justify-center hover:bg-padel-blue-light transition-colors">
                 <Instagram className="w-5 h-5" />
               </a>
               
